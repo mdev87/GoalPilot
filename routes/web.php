ac79623da3f31f21ab162cc77a6b81524ad84bec
mdev87 <saghafi.mt@gmail.com>
2026-08-01 19:48:26 +0330

    feat: register routes and update navigation sidebar

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\Auth\GoogleController;
+use App\Livewire\Goals\GoalsManager;
+use App\Livewire\TimeEntries\TimeLogger;
+use App\Livewire\Weeks\WeekPlanner;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome')->name('home');
@@ -12,6 +15,9 @@ Route::middleware('guest')->group(function () {
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
+    Route::get('goals', GoalsManager::class)->name('goals.index');
+    Route::get('weeks', WeekPlanner::class)->name('weeks.index');
+    Route::get('time-entries', TimeLogger::class)->name('time-entries.index');
 });
 
 require __DIR__.'/settings.php';
